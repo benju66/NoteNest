971fd22d4ecf86e6e2bb9aa384f6084f65a22650
benju66 <benj.urness@gmail.com>
2025-08-26 14:24:30 -0500

    tab overflow

--- NoteNest.Core/obj/Debug/net9.0/NoteNest.Core.AssemblyInfo.cs
+++ NoteNest.Core/obj/Debug/net9.0/NoteNest.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteNest.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00f103c67eeb06c27bf8d263c695e06e34c67c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468cd01a700df300e27eacdfbb5f752ab1ff8e71")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteNest.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteNest.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
